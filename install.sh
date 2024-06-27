3b16d19f4dfacfb009d4b8e0a77cd578702d6b34
Alex Sh <shalex.work@gmail.com>
2024-06-27 11:07:36 +0300

    Split installation and running

---
+++ install.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+############## Server setup ##########################
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+configure() {
+    # Backup original nginx.conf
+    if [ ! -f /etc/nginx/nginx.conf.backup ]; then
+        sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
+    fi
+
+    # Backup original default site config
+    if [ ! -f /etc/nginx/sites-available/default.backup ]; then
+        sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.backup
+    fi
+
+    cp "$SCRIPT_DIR/nginx/nginx.conf.template" "$SCRIPT_DIR/nginx/nginx.conf"
+    sed -i "s|@path@|$SCRIPT_DIR|g" "$SCRIPT_DIR/nginx/nginx.conf"
+
+    # Create symlink to the new config
+    sudo ln -sf "$SCRIPT_DIR/nginx/nginx.conf" /etc/nginx/sites-available/default
+
+    # Add the current user to the www-data group
+    sudo usermod -aG www-data $USER
+
+    # Ensure the current user can edit files and directories
+    sudo chown -R $USER:www-data "$SCRIPT_DIR/www/html"
+    sudo chmod -R 775 "$SCRIPT_DIR/www/html"
+
+    # Set execute permissions on necessary directories
+    sudo chmod o+x $HOME
+    sudo chmod o+x $SCRIPT_DIR
+}
+
+install() {
+    sudo apt update
+    sudo apt install -y nginx
+    sudo systemctl stop nginx
+    sudo systemctl disable nginx
+}
+
+# Check if Nginx is already installed
+if ! [ -x "$(command -v nginx)" ]; then
+    install
+    configure
+fi
+
+############## Back-end setup ##########################
+# Navigate to the back-end directory
+cd "$SCRIPT_DIR/back-end"
+
+install_nodejs() {
+    sudo apt-get install -y nodejs npm
+
+    # Initialize a new Node.js project if not already done
+    if [ ! -f package.json ]; then
+    npm init -y
+    fi
+
+    # Install required npm packages
+    npm install express body-parser
+}
+
+if ! [ -x "$(command -v node)" ]; then
+    install_nodejs
+fi
--- run.sh
+++ run.sh
@@ -1,82 +1,32 @@
 #!/bin/bash
 
-############## Server setup ##########################
-
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-install_nginx() {
-    sudo apt update
-    sudo apt install -y nginx
+start() {
+    sudo systemctl start nginx
+    node $SCRIPT_DIR/back-end/server.js &
+    disown
+}
+
+stop() {
     sudo systemctl stop nginx
-    sudo systemctl disable nginx
+
+    NODE_PID=$(pgrep -f "node server.js")
+    if [ -n "$NODE_PID" ]; then
+        echo "Stopping existing node server.js process (PID: $NODE_PID)"
+        kill -9 $NODE_PID
+    fi
 }
 
-# Check if Nginx is already installed
-if ! [ -x "$(command -v nginx)" ]; then
-    install_nginx
-fi
-
-# Backup original nginx.conf
-if [ ! -f /etc/nginx/nginx.conf.backup ]; then
-    sudo cp /etc/nginx/nginx.conf /etc/nginx/nginx.conf.backup
-fi
-
-# Backup original default site config
-if [ ! -f /etc/nginx/sites-available/default.backup ]; then
-    sudo cp /etc/nginx/sites-available/default /etc/nginx/sites-available/default.backup
-fi
-
-cp "$SCRIPT_DIR/nginx/nginx.conf.template" "$SCRIPT_DIR/nginx/nginx.conf"
-sed -i "s|@path@|$SCRIPT_DIR|g" "$SCRIPT_DIR/nginx/nginx.conf"
-
-# Create symlink to the new config
-sudo ln -sf "$SCRIPT_DIR/nginx/nginx.conf" /etc/nginx/sites-available/default
-
-# Add the current user to the www-data group
-sudo usermod -aG www-data $USER
-
-# Ensure the current user can edit files and directories
-sudo chown -R $USER:www-data "$SCRIPT_DIR/www/html"
-sudo chmod -R 775 "$SCRIPT_DIR/www/html"
-
-# Set execute permissions on necessary directories
-sudo chmod o+x $HOME
-sudo chmod o+x $SCRIPT_DIR
-
-# Restart Nginx to apply changes
-sudo systemctl start nginx
-
-############## Back-end setup ##########################
-# Navigate to the back-end directory
-cd "$SCRIPT_DIR/back-end"
-
-install_nodejs() {
-    sudo apt-get install -y nodejs npm
-}
-
-if ! [ -x "$(command -v node)" ]; then
-    install_nodejs
-fi
-
-# Initialize a new Node.js project if not already done
-if [ ! -f package.json ]; then
-  npm init -y
-fi
-
-# Install required npm packages
-npm install express body-parser
-
-# Ensure any existing node server is stopped
-# Find the process ID of node server.js and kill it if running
-NODE_PID=$(pgrep -f "node server.js")
-if [ -n "$NODE_PID" ]; then
-    echo "Stopping existing node server.js process (PID: $NODE_PID)"
-    kill -9 $NODE_PID
-fi
-
-# Start the server
-echo "Starting node server.js"
-node server.js &
-
-# Disown the background process to avoid hangups
-disown
+case "$1" in
+    start)
+        start
+        ;;
+    stop)
+        stop
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
