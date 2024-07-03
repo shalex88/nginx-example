4ca544aeb06aa1c434575931669e264418de7c31
Alex Sh <shalex.work@gmail.com>
2024-07-03 14:22:36 +0300

    Improve error handling

--- back-end/server.js
+++ back-end/server.js
@@ -60,29 +60,50 @@ app.get('/getTopOutput', (req, res) => {
         const memoryUsage = output.match(/RAM .*?MB/)[0];
         const swapUsage = output.match(/SWAP .*?MB/)[0];
         const cpuUsage = output.match(/CPU \[.*?\]/)[0].replace(/CPU \[|\]/g, '').split(',').map((cpu, index) => `${index + 1} - ${cpu.trim().replace("%@", "% ")}MHz`).join('\n');
-        const emcFreqMatch = output.match(/EMC_FREQ \d+%@\d+/)[0].replace("%@", "% ")+"MHz";
-        const gr3dFreq = output.match(/GR3D_FREQ \d+%@\[\d+/)[0].replace("%@[", "% ")+"MHz";
-        const videoEncoding = output.match(/NVENC .*? NVDEC off NVJPG off NVJPG1 off VIC .*? OFA off NVDLA0 off NVDLA1 off PVA0_FREQ off APE \d+/)[0]
-            .replace("%@", "% ")
-            .replace(/ NVDEC/g, '\nNVDEC')
-            .replace(/ NVJPG/g, '\nNVJPG')
-            .replace(/ NVJPG1/g, '\nNVJPG1')
-            .replace(/ VIC/g, '\nVIC')
-            .replace("%@", "% ")
-            .replace(/ OFA/g, '\nOFA')
-            .replace(/ NVDLA0/g, '\nNVDLA0')
-            .replace(/ NVDLA1/g, '\nNVDLA1')
-            .replace(/ PVA0_FREQ/g, '\nPVA0_FREQ')
-            .replace(/ APE/g, '\nAPE') + "MHz";
-        const temperatures = output.match(/cpu@.*?C tboard@.*?C soc2@.*?C tdiode@.*?C soc0@.*?C tj@.*?C soc1@.*?C/)[0]
-            .replace("@", " ")
-            .replace("@", " ")
-            .replace("@", " ")
-            .replace("@", " ")
-            .replace("@", " ")
-            .replace("@", " ")
-            .replace("@", " ")
-            .replace("@", " ");
+
+        emcFreqMatch = "N/A";
+        try {
+            emcFreqMatch = output.match(/EMC_FREQ \d+%@\d+/)[0].replace("%@", "% ")+"MHz";
+        } catch (e) {
+        }
+
+        gr3dFreq = "N/A";
+        try {
+            gr3dFreq = output.match(/GR3D_FREQ \d+%@\[\d+/)[0].replace("%@[", "% ")+"MHz";
+        } catch (e) {
+        }
+
+        videoEncoding = "N/A";
+        try {
+            videoEncoding = output.match(/NVENC .*? NVDEC off NVJPG off NVJPG1 off VIC .*? OFA off NVDLA0 off NVDLA1 off PVA0_FREQ off APE \d+/)[0]
+                .replace("%@", "% ")
+                .replace(/ NVDEC/g, '\nNVDEC')
+                .replace(/ NVJPG/g, '\nNVJPG')
+                .replace(/ NVJPG1/g, '\nNVJPG1')
+                .replace(/ VIC/g, '\nVIC')
+                .replace("%@", "% ")
+                .replace(/ OFA/g, '\nOFA')
+                .replace(/ NVDLA0/g, '\nNVDLA0')
+                .replace(/ NVDLA1/g, '\nNVDLA1')
+                .replace(/ PVA0_FREQ/g, '\nPVA0_FREQ')
+                .replace(/ APE/g, '\nAPE') + "MHz";
+        } catch (e) {
+        }
+
+        temperatures = "N/A";
+        try {
+            temperatures = output.match(/cpu@.*?C tboard@.*?C soc2@.*?C tdiode@.*?C soc0@.*?C tj@.*?C soc1@.*?C/)[0]
+                .replace("@", " ")
+                .replace("@", " ")
+                .replace("@", " ")
+                .replace("@", " ")
+                .replace("@", " ")
+                .replace("@", " ")
+                .replace("@", " ")
+                .replace("@", " ");
+        } catch (e) {
+        }
+
         const powerConsumption = output.match(/VDD_GPU_SOC .*? VDD_CPU_CV .*? VIN_SYS_5V0 .*? VDDQ_VDD2_1V8AO .*?mW.*?mW/)[0]
             .replace(/ VDD_CPU_CV/g, '\nVDD_CPU_CV')
             .replace(/ VIN_SYS_5V0/g, '\nVIN_SYS_5V0')
